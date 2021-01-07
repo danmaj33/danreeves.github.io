7c1a9b77f6dc10790c2ad740e1c02f2b742c033e
Danmaj33 <danmaj33@byui.edu>
2021-01-07 15:38:00 -0700

    bigupdate

---
+++ mainscript.js
@@ -0,0 +1,14 @@
+/* Input: when sombody acesses the site
+ * Processing: calculates the last date modified
+ * Output: last use date and time
+ */
+
+    
+let oLastModif = new Date(document.lastModified);
+document.write(oLastModif);
+// returns: Tuesday, December 16, 2017 11:09:42
+
+
+
+
+     
