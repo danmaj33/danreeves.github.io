889f7fe1b313b451eb1beb664d1cb6507df01a3e
Danmaj33 <danmaj33@byui.edu>
2021-01-28 20:27:16 -0700

    footer

--- lesson4/js/lesson4.js
+++ lesson4/js/lesson4.js
@@ -2,3 +2,12 @@ const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+const yearoptions = {year:'numeric'};
+document.getElementById('currentyear').textContent = new Date().toLocaleDateString('en-US', yearoptions);
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
