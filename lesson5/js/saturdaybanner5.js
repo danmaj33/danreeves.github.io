9dd2c2d2e870d9887de99a11c652ce535ed713a0
Danmaj33 <danmaj33@byui.edu>
2021-02-05 19:42:16 -0700

    push

--- lesson5/js/saturdaybanner5.js
+++ lesson5/js/saturdaybanner5.js
@@ -1,8 +1,13 @@
-let today = new Date();
-
-if(today.getDay() == 4){
-    document.getElementById('satbanner').innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
-};
 
 
 
+function satbanner() {
+    let today = new Date();
+    console.log(today.getDay())
+    if(today.getDay() == 5){
+        document.getElementById('bannertext').innerHTML = 
+        '<h3>Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.</h3>';
+    } else {
+    const bannershow = document.getElementById('bannertext');
+    {bannershow.classList.add('notfriday')}
+    }
