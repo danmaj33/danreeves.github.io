6cc6a2600fbbcd6968791654b2f7233907537525
Danmaj33 <danmaj33@byui.edu>
2021-02-05 19:31:17 -0700

    java

--- lesson5/js/saturdaybanner5.js
+++ lesson5/js/saturdaybanner5.js
@@ -1,6 +1,6 @@
 let today = new Date();
 
-if(today.getDay() == 5){
+if(today.getDay() == 4){
     document.getElementById('satbanner').innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
 };
 
