76d56c91c318dd212b1dc61869252630f0f0f199
Danmaj33 <danmaj33@byui.edu>
2021-02-11 12:18:07 -0700

    banner push

--- lesson5/js/saturdaybanner5.js
+++ lesson5/js/saturdaybanner5.js
@@ -10,3 +10,15 @@ function satbanner() {
     {bannershow.classList.add('notfriday')}
     }
 }
+
+// function insertbanner() {
+//     let today = new Date();
+//     console.log(today.getDay())
+//     if(today.getDay() == 5){
+//         const bannerhide = document.getElementById('bannertext');
+//         {bannerhide.classList.remove('notfriday')}
+//     } else {
+//     const bannershow = document.getElementById('bannertext');
+//     {bannershow.classList.add('notfriday')}
+//     }
+// }
