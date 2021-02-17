8bdbdeb2b12a39aac23aa4590705f9ba7d105ef3
Danmaj33 <danmaj33@byui.edu>
2021-02-16 19:59:13 -0700

    push

--- lesson6/js/windchill6.js
+++ lesson6/js/windchill6.js
@@ -17,10 +17,14 @@
 }
 
 
+
 // call the second function (windChill)
 // store the value returned by windChill in a variable
 // output the value returned by windChill to the div for the user to see
 function windChill(T, S) {
+     if (T < 50 && S > 3) {
+}
+
  let wC  =  35.74 + 0.6215*T-35.75*(Math.pow(S, 0.16)) + 0.4275*T*(Math.pow(S, 0.16));
  let rwC = Math.round(wC);
 
