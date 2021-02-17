f46839e43b0208d398507361fa21724a2baf8e9a
Danmaj33 <danmaj33@byui.edu>
2021-02-16 20:25:35 -0700

    push

--- lesson6/js/windchill6.js
+++ lesson6/js/windchill6.js
@@ -13,7 +13,7 @@
     let tempF = parseFloat(document.getElementById('highnmb').innerHTML); 
     let speed = parseFloat(document.getElementById('wndmph').innerHTML); 
    let wCOutput = windChill(tempF, speed);
-   document.getElementById("outputDiv").innerHTML= wCOutput+"F"+"&deg;";
+   document.getElementById("outputDiv").innerHTML= wCOutput +"F"+"&deg;";
 }
 
 
@@ -22,12 +22,18 @@
 // store the value returned by windChill in a variable
 // output the value returned by windChill to the div for the user to see
 function windChill(T, S) {
-     if (T < 50 && S > 3) {
-}
-
+   
+   if (T > 50 && S > 3) {
+      let rwC=("  N/A ");
+      return rwC;
+      
+      
+   }
+    
  let wC  =  35.74 + 0.6215*T-35.75*(Math.pow(S, 0.16)) + 0.4275*T*(Math.pow(S, 0.16));
  let rwC = Math.round(wC);
 
+ 
  return rwC;
 }
 
