18263147e1d64afe6a335f60c6f69a361474b124
Danmaj33 <danmaj33@byui.edu>
2021-02-18 19:08:13 -0700

    push

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -4,12 +4,14 @@ let imgsToLoad = document.querySelectorAll("[data-src]");
 const imgOptions = {
     threshold: 0,
     rootMargin: "0 0 50px 0"
+
+    // ^^^^^^ How close you need to be to load the image. 
 };
 
 const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
+    image.setAttribute("src", image.getAttribute("data-src"));
     image.onload = () => {
-        image.removeAttribute('data-src');
+        image.removeAttribute("data-src");
     };
 };
 
