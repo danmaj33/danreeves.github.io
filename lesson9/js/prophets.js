fda0ff74fc0ffa195762004da2b4e4b527d25197
Danmaj33 <danmaj33@byui.edu>
2021-03-02 17:47:23 -0700

    push

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,30 +1,38 @@
 const requestURL = 'byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
 
-
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
-
+  
 
   .then(function (jsonObject) {
+   
+   
     console.table(jsonObject);  // temporary checking for valid response and data parsing
+     
+    
+    const prophets = jsonObject['prophets'];
    
-    for (let i = 0; i < prophets.length; i++ ) { 
+                    for (let i = 0; i < prophets.length; i++ ) { 
         
-    let card = document.createElement('section');
-   
-    let h2 = document.createElement('h2');
+                                 let card = document.createElement('section');
+                                    let h2 = document.createElement('h2');
+    
+
+                                         image.setAttribute('src', prophets[i].imageurl);
     
     h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
     
+    
     card.appendChild(h2);
     
+    
     document.querySelector('div.cards').appendChild(card);
-    image.setAttribute('src', prophets[i].imageurl);
    
-});
-const prophets = jsonObject['prophets'];
+   
+}});
+
 
 
  
