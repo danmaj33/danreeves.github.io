0a1cfe7473487b7a520dc3dae523a774cb2a5c45
Danmaj33 <danmaj33@byui.edu>
2021-03-03 20:48:09 -0700

    push

---
+++ lesson9/js/fontload9.js
@@ -0,0 +1,11 @@
+
+// font-family: 'Averia Serif Libre', cursive;
+// font-family: 'Rock Salt', cursive;
+
+
+
+  WebFont.load({
+    google: {
+      families: ['Averia Serif Libre','Rock Salt']
+    }
+  });
