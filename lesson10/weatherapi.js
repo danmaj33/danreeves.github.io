50dbe05c8f5d7fab4cbddbf5c8206ad144e19657
Danmaj33 <danmaj33@byui.edu>
2021-03-11 20:19:39 -0700

    push

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -6,7 +6,7 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    // console.log(jsObject);
 
     // -id Needs a #
    
