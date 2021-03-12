fa40d6a5e24b6f5fe68a8437042aee11977f8a08
Danmaj33 <danmaj33@byui.edu>
2021-03-12 16:43:25 -0700

    push

--- lesson10/js/forecastapi.js
+++ lesson10/js/forecastapi.js
@@ -9,25 +9,70 @@ fetch(apiURL)
     // -id Needs a #
    
   const day1temp = document.querySelector('#day1temp');
-  day1temp.textContent = jsObject.list[0].main.temp;
+  day1temp.textContent = jsObject.list[0].main.temp + " F"+ "\xB0";
   const day2temp = document.querySelector('#day2temp');
-  day2temp.textContent = jsObject.list[1].main.temp;
+  day2temp.textContent = jsObject.list[14].main.temp + " F"+ "\xB0";
   const day3temp = document.querySelector('#day3temp');
-  day3temp.textContent = jsObject.list[2].main.temp;
+  day3temp.textContent = jsObject.list[21].main.temp + " F"+ "\xB0";
   const day4temp = document.querySelector('#day4temp');
-  day4temp.textContent = jsObject.list[3].main.temp;
+  day4temp.textContent = jsObject.list[29].main.temp + " F"+ "\xB0";
   const day5temp = document.querySelector('#day5temp');
-  day5temp.textContent = jsObject.list[4].main.temp;
+  day5temp.textContent = jsObject.list[37].main.temp + " F"+ "\xB0";
+
+  
+  // const date1 = document.querySelector('#date1');
+  // date1.textContent = jsObject.list[0].dt_txt;
 
  
 
   
   
   
-   const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[1].icon + '.png';  // note the concatenation
+  const imagesrc1 = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png';  // note the concatenation
+  const desc1 = jsObject.list[0].weather[0].description;  // note how we reference the weather array
+  // document.getElementById('imagesrc1').textContent = imagesrc1;  // informational specification only
+  document.getElementById('icon1').setAttribute('src', imagesrc1);  // focus on the setAttribute() method
+  document.getElementById('icon1').setAttribute('alt', desc1);
 
-   const desc = jsObject.list[0].weather[0].description;  // note how we reference the weather array
-  document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-  document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-  document.getElementById('icon').setAttribute('alt', desc);
+  const imagesrc2 = 'https://openweathermap.org/img/w/' + jsObject.list[14].weather[0].icon + '.png';  // note the concatenation
+  const desc2 = jsObject.list[0].weather[0].description;  // note how we reference the weather array
+  // document.getElementById('imagesrc2').textContent = imagesrc2;  // informational specification only
+  document.getElementById('icon2').setAttribute('src', imagesrc2);  // focus on the setAttribute() method
+  document.getElementById('icon2').setAttribute('alt', desc2);
+
+  const imagesrc3 = 'https://openweathermap.org/img/w/' + jsObject.list[21].weather[0].icon + '.png';  // note the concatenation
+  const desc3 = jsObject.list[0].weather[0].description;  // note how we reference the weather array
+  // document.getElementById('imagesrc2').textContent = imagesrc2;  // informational specification only
+  document.getElementById('icon3').setAttribute('src', imagesrc3);  // focus on the setAttribute() method
+  document.getElementById('icon3').setAttribute('alt', desc3);
+
+  const imagesrc4 = 'https://openweathermap.org/img/w/' + jsObject.list[29].weather[0].icon + '.png';  // note the concatenation
+  const desc4 = jsObject.list[0].weather[0].description;  // note how we reference the weather array
+  // document.getElementById('imagesrc2').textContent = imagesrc2;  // informational specification only
+  document.getElementById('icon4').setAttribute('src', imagesrc4);  // focus on the setAttribute() method
+  document.getElementById('icon4').setAttribute('alt', desc4);
+
+  const imagesrc5 = 'https://openweathermap.org/img/w/' + jsObject.list[37].weather[0].icon + '.png';  // note the concatenation
+  const desc5 = jsObject.list[0].weather[0].description;  // note how we reference the weather array
+  // document.getElementById('imagesrc2').textContent = imagesrc2;  // informational specification only
+  document.getElementById('icon5').setAttribute('src', imagesrc5);  // focus on the setAttribute() method
+  document.getElementById('icon5').setAttribute('alt', desc5);
+
+  const daydesc1 = document.querySelector('#daydesc1');
+  daydesc1.textContent = jsObject.list[0].weather[0].description;
+
+  const daydesc2 = document.querySelector('#daydesc2');
+  daydesc2.textContent = jsObject.list[14].weather[0].description;
+
+  constdaydesc3 = document.querySelector('#daydesc3');
+  daydesc3.textContent = jsObject.list[21].weather[0].description;
+
+  const daydesc4 = document.querySelector('#daydesc4');
+  daydesc4.textContent = jsObject.list[29].weather[0].description;
+
+  const daydesc5 = document.querySelector('#daydesc5');
+  daydesc5.textContent = jsObject.list[37].weather[0].description;
+
+
+ 
 });
